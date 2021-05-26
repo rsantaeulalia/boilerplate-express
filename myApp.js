c209b7947d60f5ac2f1c1df3e9dfa5ae8863ebc4
Rodrigo Santa Eulalia <rsantaeulalia@medallia.com>
2021-05-26 00:00:02 -0300

    added get endpoint

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 console.log("Hello world");
 
-app.get("/", (req, res) => { res.send("Hello world") })
+app.get("/", (req, res) => { res.send("Hello Express") })
 
 
 
